8c838331f3bc8ca4cc05a19e860afbfdd1f25833
Nick <111998485+NicholasEng26@users.noreply.github.com>
2022-10-13 12:15:53 -0500

    Polish flaggggggg

--- src/studio4/Flag.java
+++ src/studio4/Flag.java
@@ -1,11 +1,18 @@
 package studio4;
 
 import java.awt.Color;
-
 import edu.princeton.cs.introcs.StdDraw;
 
 public class Flag {
 	public static void main(String[] args) {
+		
+		StdDraw.setPenColor(Color.BLACK);
+		StdDraw.setPenRadius(0.005);
+		StdDraw.rectangle(0.5,0.7, 0.45,0.2);
+		
+		StdDraw.setPenColor(Color.RED);
+		StdDraw.setPenRadius(0.002);
+		StdDraw.filledRectangle(0.5,0.3, 0.45,0.2);
 		
 	}
 }
