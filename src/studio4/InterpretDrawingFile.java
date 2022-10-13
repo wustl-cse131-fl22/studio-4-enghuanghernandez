3f8711944695fd864e921a42706da4186ebaff7f
Nick <111998485+NicholasEng26@users.noreply.github.com>
2022-10-13 11:48:47 -0500

    Made purple Ellipse

--- src/studio4/InterpretDrawingFile.java
+++ src/studio4/InterpretDrawingFile.java
@@ -1,5 +1,6 @@
 package studio4;
 
+import java.awt.Color;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
@@ -18,7 +19,30 @@ public class InterpretDrawingFile {
 		JFileChooser chooser = new JFileChooser("resources");
 		chooser.showOpenDialog(null);
 		File f = new File(chooser.getSelectedFile().getPath());
-		Scanner in = new Scanner(f); //making Scanner with a File
-		
+		Scanner in = new Scanner(f); // making Scanner with a File
+
+		StdDraw.setPenColor(Color.BLACK);
+		StdDraw.setPenRadius(0.05);
+
+		String shape = in.next();
+		int redComponent = in.nextInt();
+		int greenComponent = in.nextInt();
+		int blueComponent = in.nextInt();
+		boolean isFilled = in.nextBoolean();
+		double parameter1 = in.nextDouble();
+		double parameter2 = in.nextDouble();
+		double parameter3 = in.nextDouble();
+		double parameter4 = in.nextDouble();
+
+		if (shape.equals("ellipse")) {
+			if (isFilled) {
+				StdDraw.filledEllipse(parameter1, parameter2, parameter3, parameter4);
+			} else {
+				StdDraw.setPenColor(redComponent, greenComponent, blueComponent);
+				StdDraw.ellipse(parameter1, parameter2, parameter3, parameter4);
+			}
+		}
+		// System.out.println(shape + " " + redComponent + " " + isFilled);
+
 	}
 }
